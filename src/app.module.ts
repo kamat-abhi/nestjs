7208d46356230a3ac63452a36b572b7c5ccfc9b0
abhishek kamat <abhishekkamat142@gmail.com>
2026-01-20 11:09:24 +0530

    start learning postgersql and connect it synchronously

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { UserModule } from './users/users.module';
 import { TweetModule } from './tweet/tweet.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, TweetModule, AuthModule],
+  imports: [
+    UserModule,
+    TweetModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      entities: [],
+      synchronize: true,
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'nestjs',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
