a811e5159f113472518320397fa9a8e3228d779a
abhishek kamat <abhishekkamat142@gmail.com>
2026-01-31 23:37:30 +0530

    Add signup api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from '../users/dtos/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signup(@Body() createUserDto: CreateUserDto) {
+    return await this.authService.signup(createUserDto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from '../users/users.service';
+import { CreateUserDto } from '../users/dtos/create-user.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly userService: UserService) {}
+
+  public async signup(createUserDto: CreateUserDto) {
+    return await this.userService.createUser(createUserDto);
+  }
 }
