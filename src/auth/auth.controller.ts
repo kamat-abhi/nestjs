f6f41875d137bd5a18d4c1e02632d760006bc122
abhishek kamat <abhishekkamat142@gmail.com>
2026-02-15 09:30:31 +0530

    Add AllowAnonymous decorator and update AuthorizeGuard for public access handling

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,16 +2,19 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dtos/create-user.dto';
 import { LoginDto } from './dto/login.dto';
+import { AllowAnonymous } from './decorators/allow-anonymous.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @AllowAnonymous()
   @Post('signup')
   async signup(@Body() createUserDto: CreateUserDto) {
     return await this.authService.signup(createUserDto);
   }
 
+  @AllowAnonymous()
   @Post('login')
   @HttpCode(HttpStatus.OK)
   async login(@Body() loginDto: LoginDto) {
--- src/auth/guards/authorize.guards.ts
+++ src/auth/guards/authorize.guards.ts
@@ -9,6 +9,7 @@ import type { ConfigType } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import authConfig from '../config/auth.config';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class AuthorizeGuard implements CanActivate {
@@ -16,8 +17,19 @@ export class AuthorizeGuard implements CanActivate {
     private readonly jwtService: JwtService,
     @Inject(authConfig.KEY)
     private readonly authConfiguration: ConfigType<typeof authConfig>,
+
+    private readonly reflector: Reflector,
   ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const request: Request = context.switchToHttp().getRequest();
     const token = request.headers.authorization?.split(' ')[1];
     console.log(token);
